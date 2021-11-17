4a4a976d7350d2fe0f8ee63c7770032004ec8555
James Reid-Smith <james.reidsmith@shopify.com>
2021-11-17 11:54:43 -0500

    Initial setup with test and server working

---
+++ sorbet/tapioca/require.rb
@@ -0,0 +1,9 @@
+# typed: true
+# frozen_string_literal: true
+
+require "active_support/core_ext/integer/time"
+require "bootsnap/setup"
+require "bundler/setup"
+require "rails/all"
+require "rails/test_help"
+require "shopify/types/setup"
